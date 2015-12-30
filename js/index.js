1b91e237ff6863c73a368a4d01a63ac79330547e
codeocelot <joeygracey@gmail.com>
2015-12-29 16:53:05 -0800

    initial commit,
    created node & graph components

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import Todo from './components/todo'
+import Todo from './components/todo';
+import Graph from './components/graph';
 
 ReactDOM.render(
   <App>
-    <Todo/>
+    <Graph/>
   </App>,
   document.getElementById('react-app')
 )
