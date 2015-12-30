f45ed406c85f70d3c419f8bfb83b230c908620bf
codeocelot <joeygracey@gmail.com>
2015-12-30 15:20:22 -0800

    checkin

--- js/stores/store.js
+++ js/stores/store.js
@@ -1,32 +1,34 @@
 import Reflux from 'reflux'
 import actions from '../actions/actions'
-import _ from 'underscore'
+import _ from 'lodash'
+
+// import _ from 'underscore'
 
 export default Reflux.createStore({
   init(){
-    this.objs = [];
-    this.currId = 0;
+    this.root = null;
+    this.nodes= [
+      {k:'asdf',v:'content',c:[]}
+    ]
+    // {k:'asdf',v:'content',c:[
+    //   {k:'qwer',v:'second',c:[]}
+    // ]};
     this.listenToMany(actions);
+    this.trigger(this.nodes);
+    console.log(this.nodes)
   },
-  onAdd(obj){
-    if(!obj.content){
-      this.error = 'Cannot add empty string'
-      this.trigger(this.error);
-      return;
-    }
-    obj.id = this.currId++;
-    this.objs.push(obj);
-    // trigger an update
-    this.trigger(null,this.objs)
+  onCreate(parentID){
+    var node = {k:_.uniqueId(),v:'',c:[]}
+    // node.key = _.uniqueId();
+    var parent = _.find(this.nodes,n=>parentID === n.k);
+    debugger;
+    parent.c.push(node)
+    this.nodes[node.key] = node;
+    if( !this.root ) this.root = node;
+    this.trigger(this.nodes);
   },
-  onRemove(id){
-    this.objs = _.without(
-      this.objs,
-      _.findWhere(
-        this.objs,
-        {id}
-      )
-    );
-    this.trigger(null,this.objs);
+  onUpdate(node){
+    var n = _.find(this.nodes,n=>n.k === node.k);
+
   }
 })
